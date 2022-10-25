305ca5fc4b9db7333eca7f48752dfcd56e5fe1c5
Patryk Szafranek <patryk.szafranek94@gmail.com>
2022-10-25 13:13:40 +0200

    task 2 - done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingr = document.querySelector("#ingredients");
+ingredients.map((item) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = item;
+  listItem.classList.add("item");
+  ingr.append(listItem);
+});
+
+console.log(ingr.children);
