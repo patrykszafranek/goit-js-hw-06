7991ceea29ae67d7ad3a4f7799b7a0be2ae99ca7
Patryk Szafranek <patryk.szafranek94@gmail.com>
2022-11-01 11:54:14 +0100

    task 8 - done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
 
+const formHandler = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Wymagany EMAIL oraz HASŁO do zalogowania!");
+  } else {
+    const loginData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(loginData);
+    event.currentTarget.reset();
+  }
+};
+
+loginForm.addEventListener("submit", formHandler);
